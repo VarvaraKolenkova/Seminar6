b52ab8cc72600d524ff4623a50e82084751c1abd
Varvara.Kolenkova <var.kolenkova@yandex.ru>
2022-07-31 08:04:04 +0300

    Количество положительных чисел введенного массива

---
+++ Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿//Количество положительных элементов введенного массива
+void GetArray(string[] args)
+{
+    int size;
+    Console.WriteLine("Введите число элементов массива: ");
+    size = Convert.ToInt32(Console.ReadLine());
+    
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+       Console.WriteLine("Введите каждый элемент массива в виде целых положительных и отрицательных чисел по очереди: ");
+       array[i] = int.Parse(Console.ReadLine());
+    }        
+    Console.WriteLine(String.Join(", ", array));
+    
+    void QuantityPositiveNumbers(int[] array)
+    {
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0) count+=1;
+    }
+    Console.WriteLine($"Количество положительных элементов массива: {count}");
+    }
+    QuantityPositiveNumbers(array);
+}
+GetArray(args);
