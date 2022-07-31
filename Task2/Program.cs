e342cc4b9cf074fdc2e90765162d6e7a388d67ae
Varvara.Kolenkova <var.kolenkova@yandex.ru>
2022-07-31 09:15:51 +0300

    Точка пересечения двух прямых

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа, которая находит точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+Console.WriteLine("Введите число b1: ");
+double b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите число k1: ");
+double k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b2: ");
+double b2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите число k2: ");
+double k2 = double.Parse(Console.ReadLine());
+
+double x = (b1-b2)/(k2-k1);
+double y = k2*x+b2;
+    
+Console.WriteLine($"({x}; {y})");
